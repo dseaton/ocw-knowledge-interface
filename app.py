9e7ca933906510823cfb9a0fada86e607557bd7e
Daniel Seaton <dseaton@Daniels-MBP-66.hsd1.ma.comcast.net>
2020-07-30 11:35:01 -0400

    style updates

--- app.py
+++ app.py
@@ -8,10 +8,17 @@ app = Flask(__name__)
 model = Doc2Vec.load("data/d2v.model")
 lookup_df = pd.read_csv("data/lookup_df.csv")
 find_by_examples = [
-    "deep_learning", "maxwell", "computation", 
-    "calculus", "astrophysics", "electromagnetism", 
+    "deep_learning", "computation", "artificial_intelligence",
+    "astrophysics", "electromagnetism", "maxwell", 
     "relativity", "thermodynamics", "gluon",
-    "economics", 
+    "statistics", "statistical_mechanics",
+    "programming", "electronics", "signal_processing", "nanotechnology", 
+    "architecture", "music",
+    "medical_imaging", "literature", "fiction",
+    "linear_algebra", "topology", "calculus", "probability", 
+    "biophysics", "organic_chemistry", "anthropology",
+    "econometrics", "economics", "project_management", "innovation",
+    # "social_justice", # "urban_planning", #"educational_technology",
 ]
 
 def _get_similar_video_clips(input_term, topn=5):
@@ -22,17 +29,21 @@ def _get_similar_video_clips(input_term, topn=5):
         lookup_df.iloc[i]
         similar_video_clips.append([
             lookup_df.iloc[i]['youtube_id'],
-            lookup_df.iloc[i]['course_id'],
+            lookup_df.iloc[i]['course_num'],
+            lookup_df.iloc[i]['course_title'],
             lookup_df.iloc[i]['start']
         ])
     return similar_video_clips
 
-def _get_similar_words(input_term, topn=10):
+def _get_similar_words(input_term, topn=12):
     return [w for w, score in model.wv.most_similar(input_term, topn=topn)]
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template(
+        "index.html",
+        examples = find_by_examples,
+    )
 
 @app.route("/words/<input_term>")
 def words(input_term="deep_learning"):
