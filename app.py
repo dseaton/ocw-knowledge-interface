99ab74ff69f329381cedf6ae693d3aa5b180dcc9
Daniel Seaton <dseaton@Daniels-MBP-66.hsd1.ma.comcast.net>
2020-08-10 11:27:38 -0400

    template/style updates plus a new network visualization. Committing this working version before stepping back to remove the bigram transformer from the repo. Cleaning/prep work will be done ahead of time to avoid memory issues on heroku.

--- app.py
+++ app.py
@@ -1,25 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 from gensim.models.doc2vec import Doc2Vec
+from gensim.models.phrases import Phraser
+import networkx as nx
 import pandas as pd
+
+import input_terms
 
 app = Flask(__name__)
 
 model = Doc2Vec.load("data/d2v.model")
+bigram_transformer = Phraser.load("data/bigram_transformer_model.pkl")
 lookup_df = pd.read_csv("data/lookup_df.csv")
-find_by_examples = [
-    "deep_learning", "computation", "artificial_intelligence",
-    "astrophysics", "electromagnetism", "maxwell", 
-    "relativity", "thermodynamics", "gluon",
-    "statistics", "statistical_mechanics",
-    "programming", "electronics", "signal_processing", "nanotechnology", 
-    "architecture", "music",
-    "medical_imaging", "literature", "fiction",
-    "linear_algebra", "topology", "calculus", "probability", 
-    "biophysics", "organic_chemistry", "anthropology",
-    "econometrics", "economics", "project_management", "innovation",
-    # "social_justice", # "urban_planning", #"educational_technology",
-]
+
+find_by_examples = [input_terms.gensim_custom_preprocess(t) for t in input_terms.find_by_examples]
+find_by_examples = [bigram_transformer[e][0] for e in find_by_examples]
+
+find_by_topic_second_level = [bigram_transformer[input_terms.gensim_custom_preprocess(t)][0] for t in input_terms.find_by_topic_second_level]
 
 def _get_similar_video_clips(input_term, topn=5):
     sim_docs = model.docvecs.most_similar([model.wv.get_vector(input_term)], topn=topn)
@@ -39,6 +36,46 @@ def _get_similar_video_clips(input_term, topn=5):
 def _get_similar_words(input_term, topn=12):
     return [w for w, score in model.wv.most_similar(input_term, topn=topn)]
 
+@app.route("/network_data/<input_term>")
+def network_data(input_term="linear_algebra", topn=10, first_neighbors=5):
+    G = nx.Graph()
+    sims = model.wv.most_similar(input_term, topn=topn)
+
+    G.add_node(input_term, group="1")
+    for w,s in sims:
+        G.add_node(w, group="2")
+        G.add_edge(input_term, w, weight=s) # model.wv.wmdistance(input_term, w))
+    
+    for f in range(0, first_neighbors):
+        for w,s in model.wv.most_similar(sims[f][0], topn=topn):
+            if G.has_node(w) == False: 
+                G.add_node(w, group=str(f+3))
+            G.add_edge(sims[f][0], w, weight=s) # model.wv.wmdistance(sims[0][0], w))
+    
+    # pos = nx.spring_layout(G)
+
+    return jsonify(nx.json_graph.node_link_data(G))
+
+@app.route('/network', defaults={'input_term': 'einstein'})
+@app.route("/network/<input_term>")
+def network(input_term="einstein"):
+    return render_template(
+        "network.html",
+        input_term = input_term,
+    )
+
+@app.route('/topics', defaults={'input_term': 'deep_learning'})
+@app.route("/topics/<input_term>")
+def topics(input_term="deep_learning"):
+    return render_template(
+        "topics.html", 
+        input_term = input_term,
+        topics = find_by_topic_second_level, 
+        similar_words = _get_similar_words(input_term),
+        similar_video_clips = _get_similar_video_clips(input_term)
+    )
+
+@app.route('/words', defaults={'input_term': 'deep_learning'})
 @app.route("/words/<input_term>")
 def words(input_term="deep_learning"):
     return render_template(
